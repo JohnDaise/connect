867a399bfe9c1bf88aebf901895cdaa582df3eac
JohnDaise <jdaise@gmail.com>
2019-08-26 07:21:07 -0400

    Updates

---
+++ precache-manifest.3a175bd3d11a1c7aa025fae4f64b56be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "056413aa380cd1ea3854d040844a7c12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58a894209649d97c414b",
+    "url": "/static/css/main.a489e722.chunk.css"
+  },
+  {
+    "revision": "1964ed70ab4eceff54e5",
+    "url": "/static/js/2.9a49a14e.chunk.js"
+  },
+  {
+    "revision": "58a894209649d97c414b",
+    "url": "/static/js/main.56e1e93b.chunk.js"
+  },
+  {
+    "revision": "07e9f7917002f7dec01a",
+    "url": "/static/js/runtime~main.07adbb1a.js"
+  }
+]);
